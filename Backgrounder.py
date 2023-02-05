c7918193043c2faa5a7174ea9b333a72ae58a483
Sim0n-King <124479627+Sim0n-King@users.noreply.github.com>
2023-02-04 22:19:35 -0800

    Add files via upload

---
+++ Backgrounder.py
@@ -0,0 +1,67 @@
+import pygame
+import os
+from scipy.signal import convolve2d
+import numpy as np
+
+pygame.font.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 900,500
+WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #setting bounds of game window
+pygame.display.set_caption("Groots in Paris ") #name of the game
+
+FPS = 30
+#BORDER = pygame.Rect(WIDTH, HEIGHT)
+
+Background_Image = pygame.image.load(
+    os.path.join('ASSETS', 'Title.png')
+)
+
+# for update_pos
+kernel = np.array([
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+    ])
+
+def draw_window():
+    WIN.blit(Background_Image, (0,0))
+    pygame.display.update()
+
+def update_pos(current):
+
+    died = current*convolve2d(current, kernel, 'same')
+    died[(died < 2) + (died > 3)] = 0
+
+    return (((current == 0)*convolve2d(current, kernel, 'same') == 3) + died != 0)*1
+
+def main ():
+    run = True
+    clock = pygame.time.Clock()
+    
+    # temporary starting input
+    current = np.array([
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0],
+        [0,0,0,1,1,1,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        ])
+
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+        # current = update_pos(current)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
