a3d7c864be56075daa7f3204fbff3a279ba31437
Damien Hodsall <damien.hodsall@gmail.com>
2023-02-04 18:56:34 -0800

    added update_pos in main

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ WIDTH, HEIGHT = 900,500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #setting bounds of game window
 pygame.display.set_caption(" Name Pending ") #name of the game
 
+def update_pos(current):
+
+    died = current*convolve2d(current, kernel, 'same')
+    died[(died < 2) + (died > 3)] = 0
+
+    return (((current == 0)*convolve2d(current, kernel, 'same') == 3) + died != 0)*1
 
 def main ():
     run = True
@@ -16,7 +22,7 @@ def main ():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
+
     pygame.quit()
 
 if __name__ == "__main__":
